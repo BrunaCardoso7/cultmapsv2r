aaf9dc92022a76274daacbcb4f0c8dfeff01a436
BrunaCardoso7 <obitadrawing@gmail.com>
2023-11-16 20:06:10 -0300

    feat: organization of folders

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>ola react</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/Datas.js
@@ -0,0 +1,112 @@
+export const evento = [
+    {
+        nome: "evento 1 - culmaps ",
+        author: "Nome do Autor do evento",
+        descricao: `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical 
+        Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College 
+        in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the 
+        cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 
+        1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise
+        on the theory of ethics, very popular during the Renaissance.
+        The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.`,
+        data: "22 - 03 - 2023",
+        categoria: "Tecnologia Animes e Geeks",
+        img: "https://images.sympla.com.br/65404fd033d51-lg.png",
+        user: "CM enterprise",
+        like: "3",
+        comentario: "massa demais",
+        createdAt: "23-05-2023"
+    },
+    {
+        nome: "evento 2 - culmaps ",
+        author: "Nome do Autor do evento",
+        descricao: `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical 
+        Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College 
+        in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the 
+        cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 
+        1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise
+        on the theory of ethics, very popular during the Renaissance.
+        The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.`,
+        data: "22 - 03 - 2023",
+        categoria: "Tecnologia Animes e Geeks",
+        img: "https://images.sympla.com.br/65404fd033d51-lg.png",
+        user: "CM enterprise",
+        like: "3",
+        comentario: "massa demais",
+        createdAt: "23-05-2023"
+    },
+    {
+        nome: "evento 3 - culmaps ",
+        author: "Nome do Autor do evento",
+        descricao: `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical 
+        Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College 
+        in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the 
+        cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 
+        1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise
+        on the theory of ethics, very popular during the Renaissance.
+        The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.`,
+        data: "22 - 03 - 2023",
+        categoria: "Tecnologia Animes e Geeks",
+        img: "https://images.sympla.com.br/65404fd033d51-lg.png",
+        user: "CM enterprise",
+        like: "3",
+        comentario: "massa demais",
+        createdAt: "23-05-2023"
+    },
+    {
+        nome: "evento 4 - culmaps ",
+        author: "Nome do Autor do evento",
+        descricao: `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical 
+        Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College 
+        in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the 
+        cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 
+        1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise
+        on the theory of ethics, very popular during the Renaissance.
+        The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.`,
+        data: "22 - 03 - 2023",
+        categoria: "Tecnologia Animes e Geeks",
+        img: "https://images.sympla.com.br/65404fd033d51-lg.png",
+        user: "CM enterprise",
+        like: "3",
+        comentario: "massa demais",
+        createdAt: "23-05-2023"
+    },
+    {
+        nome: "evento 5 - culmaps ",
+        author: "Nome do Autor do evento",
+        descricao: `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical 
+        Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College 
+        in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the 
+        cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 
+        1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise
+        on the theory of ethics, very popular during the Renaissance.
+        The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.`,
+        data: "22 - 03 - 2023",
+        categoria: "Tecnologia Animes e Geeks",
+        img: "https://images.sympla.com.br/65404fd033d51-lg.png",
+        user: "CM enterprise",
+        like: "3",
+        comentario: "massa demais",
+        createdAt: "23-05-2023"
+    },
+    {
+        nome: "evento 6 - culmaps ",
+        author: "Nome do Autor do evento",
+        descricao: `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical 
+        Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College 
+        in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the 
+        cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 
+        1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise
+        on the theory of ethics, very popular during the Renaissance.
+        The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.`,
+        data: "22 - 03 - 2023",
+        categoria: "Tecnologia Animes e Geeks",
+        img: "https://images.sympla.com.br/65404fd033d51-lg.png",
+        user: "CM enterprise",
+        like: "3",
+        comentario: "massa demais",
+        createdAt: "23-05-2023"
+    },
+
+
+]
